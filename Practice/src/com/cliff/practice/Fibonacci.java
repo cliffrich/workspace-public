7677d093f996984af77a2edef90b56ddb6eed477
cliffrich <cliffra@gmail.com>
2015-03-19 18:42:45 +0000

    Refactored and added comment

--- Practice/src/com/cliff/practice/Fibonacci.java
+++ Practice/src/com/cliff/practice/Fibonacci.java
@@ -3,51 +3,41 @@ package com.cliff.practice;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author cliffr
+ * 
+ * Prints all fibonacci numbers upto the given number
+ * Has two methods doing the same job, one in a more traditional manner and the other in a recursive manner
+ * 
+ * For eg., if the input is 140, this program will print the 
+ * Fibonacci sequences upto 140
+ *
+ */
 public class Fibonacci {
 	
-	public static void getFibonacciRecursive(List<Integer> result, int start, int currentSum, int upto){
-		while(currentSum<upto){
-			if(start<2)
-				result.add(start, start);
-			else
-				result.add(start, result.get(start-1) + result.get(start-2));
-			getFibonacciRecursive(result, start+1, result.get(start), upto);
-		}
+	public static void getFibonacciRecursive(List<Integer> result, int currentIndex, int upto){
+		int newSum = (currentIndex<2) ? currentIndex : (result.get(currentIndex-1) + result.get(currentIndex-2));
+		if (newSum > upto) 
+			return;
+		result.add(currentIndex, newSum);	
+		getFibonacciRecursive(result, currentIndex+1, upto);
 	}
 	public static List<Integer> getFibonacci(int upto){
 		List<Integer> result = new ArrayList<Integer>();
-		int sum = 0, count = 0;
-		while(sum<upto){
-			if(count<2)
-				sum = count;
-			else
-				sum = result.get(count-1) + result.get(count-2);
-			if(sum>upto)
+		int newSum = 0, currentIndex = 0;
+		while(newSum<upto){
+			newSum = (currentIndex<2) ? currentIndex : (result.get(currentIndex-1) + result.get(currentIndex-2));
+			if(newSum>upto)
 				break;
-			result.add(count, sum);
-			count++;
+			result.add(currentIndex, newSum);
+			currentIndex++;
 		}
 		return result;
 	}
-	/*public static void printFibonacci(int upto){
-		int[] fibArr = new int[upto];
-		int sum = 0, count = 0;
-		for(;count<=upto;count++){
-			if(count<2)
-				sum = count;
-			else
-				sum = fibArr[count-1] + fibArr[count-2];
-			if(sum>upto)
-				break;
-			fibArr[count] = sum;
-		}
-		for(int i=0;i<count;i++){
-			System.out.println(fibArr[i]);
-		}
-	}*/
+	
 	public static void main(String[] args) {
 		List<Integer> result = new ArrayList<Integer>();
-		getFibonacciRecursive(result, 0, 0, 140);
+		getFibonacciRecursive(result, 0, 140);
 		//List<Integer> result = getFibonacci(140);
 		for(Integer item:result){
 			System.out.println(item);		
